2ec99b2804ec4f58abd996518b286308b42abfad
idheerumehra <97719920+idheerumehra@users.noreply.github.com>
2022-04-27 08:30:33 +0530

    made a minor change

--- app/src/main/java/com/dheeraj/newsxavier/ui/main/MainViewModelFactory.kt
+++ app/src/main/java/com/dheeraj/newsxavier/ui/main/MainViewModelFactory.kt
@@ -2,16 +2,15 @@ package com.dheeraj.newsxavier.ui.main
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.dheeraj.newsxavier.preferences.AppPreferences
 import com.dheeraj.newsxavier.localdata.BookmarksRepository
+import com.dheeraj.newsxavier.preferences.AppPreferences
 import java.lang.IllegalArgumentException
 
-class MainViewModelFactory(private val repository: BookmarksRepository, private val preferences : AppPreferences) : ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+class MainViewModelFactory(val repository: BookmarksRepository, val preferences: AppPreferences): ViewModelProvider.Factory{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(MainViewModel::class.java)){
-            return MainViewModel(repository, preferences) as T
+            return MainViewModel(repository,preferences) as T
         }
         throw IllegalArgumentException("Unknown View Model class")
     }
-
 }
